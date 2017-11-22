ce3d01094f83f5b562078354ff4a0f4ee8c702a4
Iordache Livia <iordacheliviaserena@gmail.com>
2017-11-22 20:58:59 +0200

    Added pixelcopter v2 env

--- ple/games/__init__.py
+++ ple/games/__init__.py
@@ -6,6 +6,7 @@ from ple.games.catcher import Catcher
 from ple.games.flappybird import FlappyBird
 from ple.games.monsterkong import MonsterKong
 from ple.games.pixelcopter import Pixelcopter
+from ple.games.pixelcopter_v2 import Pixelcopter_v2
 from ple.games.pong import Pong
 from ple.games.puckworld import PuckWorld
 from ple.games.raycastmaze import RaycastMaze
